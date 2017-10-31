ba74acad6b70f9c231f52ef4d571fc879539415a
geowarin <geowarin@gmail.com>
2017-10-31 11:57:32 +0100

    Upgrade to apollo 2

--- src/utils/graphql.js
+++ src/utils/graphql.js
@@ -1,23 +1,25 @@
-import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import ApolloClient from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
 import gql from 'graphql-tag';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ApolloLink} from 'apollo-link';
 
-const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: 'https://api.github.com/graphql'
-  })
-});
-
+const httpLink = new HttpLink({uri: 'https://api.github.com/graphql'});
 const githubToken = 'your_token_here';
 
-client.networkInterface.use([
-  {
-    applyMiddleware(req, next) {
-      if (!req.options.headers) req.options.headers = {};
-      req.options.headers.authorization = `Bearer ${githubToken}`;
-      next();
-    }
-  }
-]);
+const middlewareLink = new ApolloLink((operation, forward) => {
+    operation.setContext({
+        headers: {
+            authorization: `Bearer ${githubToken}`
+        }
+    });
+    return forward(operation);
+});
+
+const client = new ApolloClient({
+    link: middlewareLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
 
 export default client;
-export { gql };
+export {gql};
